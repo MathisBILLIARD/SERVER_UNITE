97c63ebcbf693f27f5080ad5796175a32accd2b6
Théo Pano <theopano@MacBook-Air-de-Theo.local>
2023-07-08 01:45:33 +0200

    Events

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,5 +1,66 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseInterceptors, UploadedFile, Patch, Delete } from '@nestjs/common';
+import { EventsService } from './events.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Events } from './events.entity';
+import { diskStorage } from 'multer';
+import { Observable, of } from 'rxjs';
+import * as path from 'path';
 
 @Controller('events')
-export class EventsController {}
+export class EventsController {
+    constructor(private eventsService: EventsService){}
+    @Post()
+  @UseInterceptors(FileInterceptor('image'))
+  async createConnexion(
+    @Body('id') id: string,
+    @Body('name') nom: string,
+    @Body('price') price: number,
+    @Body('image') image: string,
+    @Body('description') description: string,
+    @Body('nbConso') nbConso: number,
+  ): Promise<Events> {
+    const event = new Events();
+    event.id = id;
+    event.name = nom;
+    event.price = price;
+    event.image = image;
+    event.description = description;
+    event.nbConso = nbConso;
+    await event.save();
+    return event;
+  } 
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('image', {
+    storage: diskStorage({
+      destination: './upload',
+      filename: (req, file, cb) => {
+        const filename: string = path.parse(file.originalname).name.replace(/\s/g, '');
+        const extension: string = path.parse(file.originalname).ext;
+        cb(null, `${filename}${extension}`)
+      }
+    })
+  }))
+  upload(@UploadedFile() file): Observable<Object> {
+    return of({ imagePath: file.filename });
+  }
+
+  @Get(':id')
+  getPersonne(@Param('id') id: string) {
+    return this.eventsService.getEvent(id);
+  }
+
+  @Patch(':id')
+  updateClient(
+    @Param('id') id: string,
+    @Body() event: Events
+  ): Promise<Events> {
+    return this.eventsService.updateEvent(id, event);
+  }
+
+  @Delete(':id')
+  deleteClient(@Param('id') id: string) {
+    return this.eventsService.deleteEvent(id);
+  }
+
+}
 
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Events } from './events.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class EventsService {}
+export class EventsService {
+  constructor(
+    @InjectRepository(Events)
+    private readonly EventRepository: Repository<Events>,
+  ) {}
+  async updateEvent(id: string, event: Events) {
+    const eventUpdate = await this.EventRepository.findOne({ where: { id } });
+
+    if (!eventUpdate) {
+      throw new NotFoundException("Client doesn't exist");
+    }
+    if (event.name) {
+      eventUpdate.name = event.name;
+    }
+    if (event.image) {
+      eventUpdate.image = event.image;
+    }
+    if (event.price) {
+      eventUpdate.price = event.price;
+    }
+    if (event.nbConso) {
+      eventUpdate.nbConso = event.nbConso;
+    }
+    if (event.description) {
+      eventUpdate.description = event.description;
+    }
+    const updatedEvent = await this.EventRepository.save(eventUpdate);
+    return updatedEvent;
+  }
+
+  async deleteEvent(id: string) {
+    await this.EventRepository.delete(id);
+  }
+
+  async getEvent(id: string): Promise<Events> {
+    const event = await this.EventRepository.findOne({
+      where: { id },
+    });
+    return event;
+  }
+}

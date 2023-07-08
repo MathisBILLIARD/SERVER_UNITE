cf8fce9ba75aceacc2c2706a6ca26839b40439cf
Théo Pano <theopano@MacBook-Air-de-Theo.local>
2023-07-08 16:34:52 +0200

    UNITE

--- src/events/events.entity.ts
+++ src/events/events.entity.ts
@@ -1,6 +1,6 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('EVENTS')
+@Entity('UNITE_PARTY')
 export class Events extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: string;
